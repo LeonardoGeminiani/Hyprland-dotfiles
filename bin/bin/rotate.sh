9886d316c9f162e19f17ea2b5922280a6ebfef71
Leonardo Geminiani <leonardogeminiani7@gmail.com>
2023-10-04 16:46:48 +0200

    flip screen effect only the laptop screen

--- bin/bin/rotate.sh
+++ bin/bin/rotate.sh
@@ -1,8 +1,8 @@
 
 if [[ $(cat $HOME/.local/bin/screen_state) == 0 ]]; then
-    echo 2 > $HOME/.local/bin/screen_state
-    hyprctl keyword monitor ,preferred,auto,1,transform,2;
+    echo 2 > $HOME/.local/bin/screen_state;
+    hyprctl keyword monitor eDP-1,preferred,auto,1,transform,2;
 else
-    echo 0 > $HOME/.local/bin/screen_state
-    hyprctl keyword monitor ,preferred,auto,1,transform,0;
+    echo 0 > $HOME/.local/bin/screen_state;
+    hyprctl keyword monitor eDP-1,preferred,auto,1,transform,0;
 fi
